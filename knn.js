23b43f5287484a19bd610624714421e3da81da16
Diemisom Carlos Romano de Melo <dcarlospa@gmail.com>
2019-08-29 18:05:41 -0300

    mais testes e pre p

--- knn.js
+++ knn.js
@@ -5,8 +5,8 @@ $("#execute").click(knn);
 
 function knn(){
     console.log('foi');
-    var pessoa = '';
-    tudo = $.getJSON( "dadosTratados.json", function( data ) {
+    var pessoa = {"idade":47,"genero":0,"escolaridade":1,"recente":"Pai Rico, Pai Pobre","preferido":"O segredo de Luiza","s":[false,true,false,false,false,false,false,true,false,true,false,false,true,true,false,false,false,false,true,false,false],"m":[true,true,false,false,true,true,true,true,true,false,true,true,false,true,true,true,true,true,false,false,false,false,false],"b":[false,false,false,false,false,false,false,true,false,true,false,true,false,true,true,true,false,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false]};
+    tudo = $.getJSON( "dadosTratados2.json", function( data ) {
         var resposta = iniciaRespostas(349);
         data.forEach(element => {
             //Calcula a diferença de pessoa com element
@@ -17,8 +17,10 @@ function knn(){
       //escreve o array
       //$( "#dados" ).html( JSON.stringify(data));
     });
+}
 
-    
+function distancia(pessoa, elemento){
+    return 0;
 }
 
 function iniciaRespostas(x){
